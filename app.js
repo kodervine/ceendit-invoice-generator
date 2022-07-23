86f4d66c7c12df540efec0d2fe052e643665f409
Laskellyb <anikwenzekelly@gmail.com>
2022-07-23 22:12:50 +0100

    More work on the invoice project

---
+++ app.js
@@ -0,0 +1,10 @@
+// Declare global variable
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const invoicePage = document.querySelector('.invoice-page')
+
+//  Hide other sections on document load for only header to show
+// window.addEventListener('DOM', ()=> {
+//   main.classList.add('display-none');
+//   invoicePage.classList.add('display-none');
+// })
